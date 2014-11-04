f8dbfcae5b6cf46feb6a2238f6f4b969822badc0
Joseph Benden <joe@benden.us>
2014-11-04 09:55:02 -0700

    Fixed schema for older MySQL version.

--- test/mysql_tests.cpp
+++ test/mysql_tests.cpp
@@ -112,7 +112,7 @@ class TransactionTest : public ::testing::Test {
             connection = create_mysql_connection();
             connection->open("test", "127.0.0.1", 3306, "root", "");
             connection->execute("set time_zone='+0:00'");
-            connection->execute("CREATE TABLE testing (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, text VARCHAR(128), num INT, fl FLOAT, createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updatedOn TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) ENGINE=InnoDB");
+            connection->execute("CREATE TABLE testing (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, text VARCHAR(128), num INT, fl FLOAT, createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updatedOn TIMESTAMP) ENGINE=InnoDB");
             connection->beginTrans();
         }
 
