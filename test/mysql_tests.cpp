2f6dfccca186a47e7c8958f5b7fc9b7275fdc6c3
Joseph Benden <joe@benden.us>
2014-11-04 09:38:58 -0700

    Fixed schema.

--- test/mysql_tests.cpp
+++ test/mysql_tests.cpp
@@ -75,7 +75,7 @@ TEST_F(DefaultTest, CanConnectToDatabase) {
 }
 
 TEST_F(DefaultTest, CanExecuteSimpleQuery) {
-    EXPECT_EQ(connection->execute("CREATE TABLE testing (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, text VARCHAR(128), num INT, fl FLOAT, createdOn TIMESTAMP, updatedOn TIMESTAMP DEFAULT NULL) ENGINE=InnoDB"), true);
+    EXPECT_EQ(connection->execute("CREATE TABLE testing (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, text VARCHAR(128), num INT, fl FLOAT, createdOn TIMESTAMP, updatedOn) ENGINE=InnoDB"), true);
     EXPECT_EQ(connection->execute("DROP TABLE testing"), true);
 }
 
@@ -112,7 +112,7 @@ class TransactionTest : public ::testing::Test {
             connection = create_mysql_connection();
             connection->open("test", "127.0.0.1", 3306, "root", "");
             connection->execute("set time_zone='+0:00'");
-            connection->execute("CREATE TABLE testing (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, text VARCHAR(128), num INT, fl FLOAT, createdOn TIMESTAMP, updatedOn TIMESTAMP DEFAULT NULL) ENGINE=InnoDB");
+            connection->execute("CREATE TABLE testing (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, text VARCHAR(128), num INT, fl FLOAT, createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updatedOn TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) ENGINE=InnoDB");
             connection->beginTrans();
         }
 
